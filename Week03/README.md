b41e18b91ea4029b8b923d97aba6b3c9e70a7215
hblock <1807227469@qq.com>
2020-12-09 20:34:26 +0800

    新增Weeko3作业

--- Week03/README.md
+++ Week03/README.md
@@ -1,1 +1,5 @@
-学习笔记
+# 作业
+
+[作业](https://github.com/hbinr/Go-000/tree/main/Week03/code/homework)
+
+# 学习笔记
--- Week03/code/homework/main.go
+++ Week03/code/homework/main.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	eg := errgroup.Group{}
+	serverErr := make(chan error, 1)
+	sigC := make(chan os.Signal, 1)
+
+	s := http.Server{Addr: ":8080"}
+
+	eg.Go(func() error {
+		go func() {
+			serverErr <- s.ListenAndServe()
+		}()
+		select {
+		case err := <-serverErr:
+			close(sigC)
+			close(serverErr)
+			return err
+		}
+	})
+
+	eg.Go(func() error {
+		signal.Notify(sigC,
+			syscall.SIGINT|syscall.SIGTERM|syscall.SIGKILL)
+		<-sigC
+		return s.Shutdown(context.TODO())
+	})
+	fmt.Println("test success......")
+	log.Println(eg.Wait())
+}
